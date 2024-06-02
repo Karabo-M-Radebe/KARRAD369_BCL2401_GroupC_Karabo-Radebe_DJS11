dd0ef54021bb8774f15ba89f864eb2275cc050b3
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-06-02 22:10:49 +0200

    created a navigation bar in the components folder and exported it into the App.jsx file

---
+++ components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Navbar () {
+    return (
+        <>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Search</a></li>
+                <li><a href="/">Library</a></li>
+                <li><a href="/">Favourites</a></li>
+            </ul>
+        </nav>
+        </>
+    );
+} 
+    
