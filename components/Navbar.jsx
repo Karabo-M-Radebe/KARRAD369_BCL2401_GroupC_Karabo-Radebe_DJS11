cc60a8a9c2ca08a095c4156ab143130304febea7
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-06-02 22:28:34 +0200

    styled the Navbar elements to display in flex

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,13 +3,11 @@ import React from "react";
 export default function Navbar () {
     return (
         <>
-        <nav>
-            <ul>
+        <nav className="Navbar">
                 <li><a href="/">Home</a></li>
                 <li><a href="/">Search</a></li>
                 <li><a href="/">Library</a></li>
                 <li><a href="/">Favourites</a></li>
-            </ul>
         </nav>
         </>
     );
