b51ca0eedec699f84d1fafb913c991c94b8a241a
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-06-05 09:23:10 +0200

    built a card component that will display all the data from the API that was fetched and mapped onto the card component

---
+++ components/Card.jsx
@@ -0,0 +1,9 @@
+export const Card = ({title, description}) => {
+    return(
+        <>
+        <h1>{title}</h1>
+        <p>{description}</p>
+        </>
+    )
+
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import Navbar from '../components/Navbar';
+import {Card}  from '../components/Card';
 
 export default function App() {
+  const [allShows, setAllShows] = useState()
+  useEffect(() => {
+    fetch('https://podcast-api.netlify.app')
+    .then(response => response.json())
+    .then(data => setAllShows(data))
+
+  }, [])
 
   return (
+    <>
     <Navbar/>
+    {allShows.map((show)=> <Card title={show.title} description={show.description}/> )}
+    </>
   );
 }
 
