6cdb9068dfdc4a851105bc50a1f36675a8af5f48
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-06-18 09:11:13 +0200

    created some async fetch functions that I can abstract throughout the project. Then built a new Card function using bootstrap

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,12 +1,30 @@
 
 
-export const Card = ({image, title, description}) => {
-    return(
-        <>
-        <image>{image}</image>
-        <h1>{title}</h1>
-        <p>{description}</p>
-        </>
-    )
+// export const Card = ({image, title, description}) => {
+//     return(
+//         <>
+//         <image>{image}</image>
+//         <h1>{title}</h1>
+//         <p>{description}</p>
+//         </>
+//     )
 
+// }
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
+export const Card = (image, title, description) => {
+
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={image} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Button variant="primary">Read More</Button>
+      </Card.Body>
+    </Card>
+  );
 }
