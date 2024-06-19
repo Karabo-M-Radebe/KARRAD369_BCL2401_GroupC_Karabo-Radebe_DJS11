3373f411919ad236a3028982364b7462c74a0b2e
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-06-19 14:55:22 +0200

    removed bootstrap dependencies by uninstalling bootstrap. Then renamed the Search.jsx file to Podcasts.jsx

---
+++ presentation/Podcasts.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react"
+
+// export const Search = () => {
+//     const [searchBar, setSearchBar] = useState()
+
+//     useEffect(()=>{
+//         fetch('https://podcast-api.netlify.app')
+//         .then(response => response.json())
+//         .then(data => setSearchBar(data))
+//         .catch(error => {throw error})
+//     })
+
+//     return(
+//         <></>
+//     )
+// }
+
+export const Podcasts = () => {
+
+    return (
+        <>
+        
+        </>
+    )
+, []}
