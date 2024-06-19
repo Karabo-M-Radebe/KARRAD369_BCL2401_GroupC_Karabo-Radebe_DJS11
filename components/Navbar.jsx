1641a0aac8b365c697f1c7ecbf53920ad745a6a9
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-06-19 15:20:48 +0200

    Navbar update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,8 +5,8 @@ export const Navbar = () => {
         <>
         <nav className="Navbar">
                 <li className="li"><a href="/">Home</a></li>
-                <li className="li"><a href="/search">Search</a></li>
-                <li className="li"><a href="/library">Library</a></li>
+                <li className="li">Search</li>
+                <li className="li"><a href="/podcasts">Podcasts</a></li>
                 <li className="li"><a href="/favourites">Favourites</a></li>
         </nav>
         </>
