12bd257ae3c2fc9e251de252a3fec778df6da078
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-06-19 15:13:46 +0200

    card component update

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,48 +1,12 @@
 
 
-// export const Card = ({image, title, description}) => {
-//     return(
-//         <>
-//         <image>{image}</image>
-//         <h1>{title}</h1>
-//         <p>{description}</p>
-//         </>
-//     )
+export const Card = ({image, title, seasons}) => {
+    return(
+        <Card className='card'>
+        <image className='podcast-card-image'>{image}</image>
+        <h1>{title}</h1>
+        <p>Seasons: {seasons}</p>
+        </Card>
+    )
 
-// }
-
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import { Row } from 'react-bootstrap';
-import {Col} from 'react-bootstrap';
-
-
-
-export const CardPreview = ({image, title, seasons}) => {
-
-  return (
-    // <Card className='card' >
-    //   <Card.Img className= 'podcast-card-image' variant="top" src={image} />
-    //   <Card.Body>
-    //     <Card.Title>{title}</Card.Title>
-    //     <Card.Text>Seasons:{seasons}</Card.Text>
-    //     <Button variant="primary">Read More</Button>
-    //   </Card.Body>
-    // </Card>
-    <Row xs={1} md={2} className="g-4">
-      {Array.from({ length: Array.length }).map((_, idx) => (
-        <Col key={idx}>
-          <Card className='card'>
-            <Card.Img className='podcast-card-image' variant="top" src={image} />
-            <Card.Body>
-              <Card.Title>{title}</Card.Title>
-              <Card.Text> Seasons: {seasons} </Card.Text>
-              <Button className='card-button' Link>Read More</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
-  );
 }
-
