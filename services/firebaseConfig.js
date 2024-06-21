7d31c27da84b5f80c165158b07c047b036f00478
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-06-21 16:29:40 +0200

    configured the firebase js file to include the hooks in the app

--- services/firebaseConfig.js
+++ services/firebaseConfig.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, } from "firebase/app";
+import { initializeApp} from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -19,9 +19,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig)
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, db, analytics, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged };
+export { auth, db, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged };
