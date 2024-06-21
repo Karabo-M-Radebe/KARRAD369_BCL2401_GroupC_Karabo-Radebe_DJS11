5e8f86b56c0f471468a95f5938df086142d243f3
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-06-21 03:34:29 +0200

    Updated the Navbar and Footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,5 +1,10 @@
+import './Footer.css'
+
 export const Footer =() => {
     return (
-        <footer>&#169; 2024 BeanPod | Karabo M. Radebe</footer>
+        <div className='footer-container'>
+           <footer className="footer">&#169; 2024 BeanPod | Karabo M. Radebe</footer>
+        </div>
+        
     )
 }
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,15 +1,20 @@
 import './Navbar.css';
+import { Link, NavLink } from 'react-router-dom';
 
 
 export const Navbar = () => {
     return (
         <>
-        <nav className="Navbar">
-                <li className="li"><a href="/">BeanPod</a></li>
+     <nav className="nav">
+         <Link to="/">BeanPod</Link>
+          <NavLink className="nav-li" to="/search">Search</NavLink>
+          <NavLink className="nav-li" to="/podcasts">Podcasts</NavLink>
+          <NavLink className="nav-li" to="/favourites">Favourites </NavLink>
+                {/* <li className="li"><a href="/">BeanPod</a></li>
                 <li className="li"><a href="/search">Search</a></li>
                 <li className="li"><a href="/podcasts">Podcasts</a></li>
-                <li className="li"><a href="/favourites">Favourites</a></li>
-        </nav>
+                <li className="li"><a href="/favourites">Favourites</a></li> */}
+     </nav>
         </>
     );
 } 
