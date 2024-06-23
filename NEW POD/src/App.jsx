7de35b38fbfb9372e179ffdc501c393261250221
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-06-23 17:27:12 +0200

    file structure changes

---
+++ NEW POD/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Favourites } from "../presentation/Favourites/Favourites";
+import { Home } from "../presentation/Home/Home";
+import { Layout } from "../presentation/Layout/Layout";
+import { CardPreview } from "../components/CardPreview/CardPreview";
+import { Library } from "../presentation/Library/Library";
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/:id" element={<CardPreview />} />
+            <Route path="/library" element={<Library />} />
+            <Route path="/favourites" element={<Favourites />} /> 
+          </Route>
+        </Routes> 
+      </BrowserRouter>
+    </>
+  );
+}
